82ce436b8922ae00a9963fb6b26505c58e2205f9
hoopdead <polec0@wp.pl>
2019-09-11 23:20:56 +0200

    Add simple enemy section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+//Main character section
+
 const main_character = {
   level: 1,
   coins: 100,
@@ -6,12 +8,35 @@ const main_character = {
   agility: 0,
   inteligence: 0,
   defence: 0,
-  luck: 0
+  luck: 0,
+  hp: 100
 };
 
+//Enemy section
+
+var enemy = {
+  level: 2,
+  drop_coins: 150,
+  strenght: 3,
+  agility: 5,
+  inteligence: 3,
+  defence: 9,
+  luck: 3,
+  hp: 25
+};
+
+//Shop section
 var shop = {
   item1: {
     name: "Long Sword",
+    cost: 100
+  },
+  item2: {
+    name: "Bow",
+    cost: 100
+  },
+  item3: {
+    name: "Magic Wand",
     cost: 100
   }
 };
