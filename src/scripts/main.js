219680febc14c6a68bbeed30dd4f117abf769686
hoopdead <polec0@wp.pl>
2019-09-11 23:04:17 +0200

    Add constant object - player's character

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
-class SomeClass {
-    method() {
-        console.log('I\'m here!');
-    }
-}
+const main_character = {
+  level: 1,
+  coins: 100,
+  profession: "none",
+  strenght: 0,
+  agility: 0,
+  inteligence: 0,
+  defence: 0,
+  luck: 0
+};
 
-const test = new SomeClass;
 
-test.method();
