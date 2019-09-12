4e1bebf59c9d582e321ca38a1154494c022a450a
hoopdead <polec0@wp.pl>
2019-09-12 18:50:03 +0200

    Changes in function names, new algorithm
    Changed a main_character name - it's too long, added a simple fight algorithm, game object section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,13 @@
+//Game section
+
+var game = {
+  isFight: false,
+  round: 0
+};
+
 //Main character section
 
-var main_character = {
+var player = {
   level: 1,
   coins: 0,
   strenght: 0,
@@ -9,14 +16,21 @@ var main_character = {
   defence: 0,
   luck: 3,
   hp: 100,
-  getDamageCharacter: function() {
+  getDamage: function() {
     return (
-      (this.level * 2 +
-        this.strenght * 0.4 +
-        this.inteligence * 0.4 +
-        this.agility * 0.35) *
-      this.luck
+      Math.floor(
+        this.level * 2 +
+          this.strenght * 0.4 +
+          this.inteligence * 0.4 +
+          this.agility * 0.35
+      ) * this.luck
     );
+  },
+  checkHp: function() {
+    if (this.hp <= 0) {
+      console.log("Player dies");
+      game.isFight = false;
+    }
   }
 };
 
@@ -31,14 +45,22 @@ var enemy = {
   defence: 9,
   luck: 3,
   hp: 25,
-  getDamageEnemy: function() {
+  getDamage: function() {
     return (
-      (this.level * 1.5 +
-        this.strenght * 0.2 +
-        this.inteligence * 0.2 +
-        this.agility * 0.15) *
+      Math.floor(
+        this.level * 1.5 +
+          this.strenght * 0.2 +
+          this.inteligence * 0.2 +
+          this.agility * 0.15
+      ) *
       (0.5 * this.luck)
     );
+  },
+  checkHp: function() {
+    if (this.hp <= 0) {
+      console.log("Enemy die.");
+      game.isFight = false;
+    }
   }
 };
 
@@ -69,13 +91,39 @@ let newItems = {
 
 $("#startfight").click(function() {
   console.log("Start fight!");
-  fight();
+  console.log("================");
+  fight(game.round);
 });
 
-let fight = () => {};
+let fight = (round, isFight) => {
+  console.log("Runda: " + (game.round + 1));
+  var turn =
+    game.round % 2 === 0 ? console.log("Player") : console.log("Enemy");
+  game.isFight = true;
+};
 
+$("#basicattack").click(function() {
+  if (game.isFight == true) {
+    console.log(
+      "Player uses basic atack: " + Math.floor(player.getDamage())
+    );
+    game.round++;
+    enemy.hp -= player.getDamage();
+    console.log("Enemy hp: " + enemy.hp);
+    enemy.checkHp();
+    enemyMove();
+  } else {
+    console.log("Start fight first!");
+  }
+});
+
+let enemyMove = () => {
+  console.log("Enemy uses basic atack: " + enemy.getDamage());
+  game.round++;
+  player.hp -= enemy.getDamage();
+  console.log("Player hp: " + player.hp);
+  player.checkHp();
+};
+
+//Sekcja testowa
 shop = Object.assign({}, shop, newItems);
-
-console.log(
-  main_character.getDamageCharacter() + " " + Math.floor(enemy.getDamageEnemy())
-);
